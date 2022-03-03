09c38f17aab16ddd93469e466c80e0511a4d13ae
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-03-03 18:32:39 +0100

    refactor: cleanup

--- src/rollingBall.js
+++ src/rollingBall.js
@@ -1,4 +1,4 @@
-import isAnyArray from 'is-any-array';
+import { isAnyArray } from 'is-any-array';
 import max from 'ml-array-max';
 import mean from 'ml-array-mean';
 import min from 'ml-array-min';
